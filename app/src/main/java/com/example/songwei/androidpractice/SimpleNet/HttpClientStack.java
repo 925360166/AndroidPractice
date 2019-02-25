93bffa092615957de68d126555a1a979ff6ac14d
songwei <songwei@zhizhangyi.com>
2019-02-25 20:33:34 +0800

    fix HttpClientStack

--- app/src/main/java/com/example/songwei/androidpractice/SimpleNet/HttpClientStack.java
+++ app/src/main/java/com/example/songwei/androidpractice/SimpleNet/HttpClientStack.java
@@ -2,11 +2,22 @@ package com.example.songwei.androidpractice.SimpleNet;
 
 import android.net.http.AndroidHttpClient;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.util.Map;
 
 
 /**
@@ -59,6 +70,67 @@ public class HttpClientStack implements HttpStack {
         }
     }
 
+    /**
+     * 设置连接参数
+     *
+     * @param httpUriRequest
+     */
+    private void setConnectionParams(HttpUriRequest httpUriRequest){
+        HttpParams httpParams = httpUriRequest.getParams();
+        HttpConnectionParams.setConnectionTimeout(httpParams, mConfig.connTimeOut);
+        HttpConnectionParams.setSoTimeout(httpParams, mConfig.soTimeOut);
+    }
 
+    /**
+     * 根据请求类型创建不同的Http请求
+     *
+     * @param request
+     * @return
+     */
+    static HttpUriRequest createHttpRequest(Request<?> request){
+        HttpUriRequest httpUriRequest = null;
+        switch(request.getHttpMethod()){
+            case GET:
+                httpUriRequest = new HttpGet(request.getUrl());
+                break;
+            case DELETE:
+                httpUriRequest = new HttpDelete(request.getUrl());
+                break;
+            case POST:
+                httpUriRequest = new HttpPost(request.getUrl());
+                httpUriRequest.addHeader(Request.HEADER_CONTENT_TYPE, request.getBodyContentType());
+                setEntityIfNonEmptyBody((HttpPost)httpUriRequest, request);
+                break;
+            case PUT:
+                httpUriRequest = new HttpPut(request.getUrl());
+                httpUriRequest.addHeader(Request.HEADER_CONTENT_TYPE, request.getBodyContentType());
+                setEntityIfNonEmptyBody((HttpPut)httpUriRequest, request);
+                break;
+            default:
+                throw new IllegalStateException("Unkonwn request method.");
+        }
+        return httpUriRequest;
+    }
+
+    private static void addHeaders(HttpUriRequest httpRequest, Map<String, String> headers){
+        for(String key : headers.keySet()){
+            httpRequest.setHeader(key, headers.get(key));
+        }
+    }
+
+    /**
+     * 将请求参数设置到HttpEntity中
+     *
+     * @param httpRequest
+     * @param request
+     */
+    private static void setEntityIfNonEmptyBody(HttpEntityEnclosingRequestBase httpRequest,
+                                                Request<?> request){
+        byte[] body = request.getBody();
+        if(body != null){
+            HttpEntity entity = new ByteArrayEntity(body);
+            httpRequest.setEntity(entity);
+        }
+    }
 
 }
