f40cacb7b69c6a2c93b8a054cc1ee281c1737167
songwei <songwei@zhizhangyi.com>
2019-04-03 10:04:24 +0800

    add glide dependency

---
+++ app/src/main/java/com/example/songwei/androidpractice/Glide_practice/GlidePracticeActivity.java
@@ -0,0 +1,10 @@
+package com.example.songwei.androidpractice.Glide_practice;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class GlidePracticeActivity extends AppCompatActivity {
+
+    private static final String TAG = "GlidePracticeActivity";
+
+
+}
