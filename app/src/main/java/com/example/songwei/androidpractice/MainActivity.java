56d116afed97250d9839d1fde909c366ee33d332
songwei <925360166@qq.com>
2019-05-10 14:17:20 +0800

    add okHttp and fix compile exception in gradle

--- app/src/main/java/com/example/songwei/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/songwei/androidpractice/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.songwei.androidpractice;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
