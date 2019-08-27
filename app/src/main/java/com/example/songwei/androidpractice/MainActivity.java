8172b1da8a65ef92b6bcce5ff21f4599603bae80
songwei <songwei@zhizhangyi.com>
2019-08-27 20:03:49 +0800

    request permissions when open MainActivity

--- app/src/main/java/com/example/songwei/androidpractice/MainActivity.java
+++ app/src/main/java/com/example/songwei/androidpractice/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.songwei.androidpractice;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,13 +28,33 @@ import com.example.songwei.androidpractice.ui_optimise.UiOptimiseActivity;
 
 import com.example.songwei.androidpractice.BgTransparentTest.BgTransparentActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //声明一个数组permissions，将所有需要申请的权限都放在里面
+    private String[] permissions = new String[]{
+            Manifest.permission.READ_CALENDAR,
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
+    private int mRequestCode = 1;
+
+    //逐个判断哪些权限未授权，将未授权的权限存储到mPermissionList中
+    private List<String> mPermissionsDenied = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initPermissions();
     }
 
 
@@ -50,6 +76,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button14).setOnClickListener(this);
         findViewById(R.id.button15).setOnClickListener(this);
         findViewById(R.id.button16).setOnClickListener(this);
+    }
+
+    //动态申请权限
+    private void initPermissions(){
+        if (Build.VERSION.SDK_INT >= 23){
+            mPermissionsDenied.clear();
+            //逐个判断是否还有未通过的权限
+            for(int i = 0; i < permissions.length; i++){
+                if(ContextCompat.checkSelfPermission(this, permissions[i]) != PackageManager.PERMISSION_GRANTED){
+                    //添加未授权的权限到mPermissionsDenied中
+                    mPermissionsDenied.add(permissions[i]);
+                }
+            }
+            //申请权限
+            if(mPermissionsDenied.size() > 0){
+                ActivityCompat.requestPermissions(this, permissions, mRequestCode);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //是否有权限申请未被通过
+        boolean hasPerDenied = false;
+        if(mRequestCode == requestCode){
+            for(int i = 0; i < grantResults.length; i++){
+                if(grantResults[i] == -1){
+                    hasPerDenied = true;
+                    break;
+                }
+            }
+        }
+        //有权限未被允许
+        if(hasPerDenied){
+            Toast.makeText(this, "有权限被拒绝，请在设置页面允许所有权限，谢谢~", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //TODO: 添加测试页面时，在此补充 fixme
