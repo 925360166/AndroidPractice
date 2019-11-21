5db1b079b3d75e613e2506e3923c53c4ed62c707
songwei <925360166@qq.com>
2019-11-21 16:40:09 +0800

    add some components

---
+++ common_lib/src/main/java/com/example/common_lib/ILoginService.java
@@ -0,0 +1,13 @@
+package com.example.common_lib;
+
+import android.content.Context;
+
+/**
+ * Author: SongWei
+ * Date:   2019/11/21
+ * Usage:
+ */
+public interface ILoginService {
+
+    public void launchLoginActivity(Context context);
+}
